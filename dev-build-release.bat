e42da5003a4e2dbda893642a28d6c57f71198247
Andrey Kovalenko <ak@taucode.com>
2022-07-28 04:11:23 +0300

    Updated content for 1.3.7

--- dev-build-release.bat
+++ dev-build-release.bat
@@ -3,7 +3,7 @@ dotnet restore
 dotnet build --configuration Debug
 dotnet build --configuration Release
 
-dotnet test -c Debug .\tests\TauCode.Db.FluentMigrations.Tests\TauCode.Db.FluentMigrations.Tests.csproj
-dotnet test -c Release .\tests\TauCode.Db.FluentMigrations.Tests\TauCode.Db.FluentMigrations.Tests.csproj
+dotnet test -c Debug .\test\TauCode.Db.FluentMigrations.Tests\TauCode.Db.FluentMigrations.Tests.csproj
+dotnet test -c Release .\test\TauCode.Db.FluentMigrations.Tests\TauCode.Db.FluentMigrations.Tests.csproj
 
 nuget pack nuget\TauCode.Db.FluentMigrations.nuspec
